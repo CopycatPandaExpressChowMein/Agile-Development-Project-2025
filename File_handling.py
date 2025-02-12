217241ad976d38a97f0de69fb041ff00d33bb052
Jesper Nilsson <dalaboda.jeppe@gmail.com>
2025-02-12 11:07:09 +0100

    ya

---
+++ File_handling.py
@@ -0,0 +1,18 @@
+import pickle
+
+def save_file(data, filepath, filename):
+    with open(filepath + "/" + filename, 'w') as file:
+        file.write(data)
+
+def load_file(data, filepath, filename):
+    with open(filepath + "/" + filename, 'r') as file:
+        file.read(data)
+
+def save_bin_file(data, filepath, filename):
+    with open(filepath + "/" + filename, 'wb') as file:
+        pickle.dump(data, file)
+
+def load_bin_file(data, filepath, filename):
+    with open(filepath + "/" + filename, 'rb') as file:
+        pickle.load(data, file)
+
