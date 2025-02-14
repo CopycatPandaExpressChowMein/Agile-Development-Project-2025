82e7db3fdcbfd3f6b7e2267b48f32fd62995a211
Jesper Nilsson <70292202+CopycatPandaExpressChowMein@users.noreply.github.com>
2025-02-14 11:14:31 +0100

    Added feature for saving

--- File_handling.py
+++ File_handling.py
@@ -1,22 +1,23 @@
 import pickle
+import pathlib
 
 
-def save_file(data, filepath, filename):
+def save_file(data, filename, filepath=pathlib.Path.cwd().__str__()):
     with open(filepath + "/" + filename, 'w') as file:
         file.write(data)
 
 
-def load_file(data, filepath, filename):
+def load_file(data, filename, filepath=pathlib.Path.cwd().__str__()):
     with open(filepath + "/" + filename, 'r') as file:
         file.read(data)
 
 
-def save_bin_file(data, filepath, filename):
+def save_bin_file(data, filename, filepath=pathlib.Path.cwd().__str__()):
     with open(filepath + "/" + filename, 'wb') as file:
         pickle.dump(data, file)
 
 
-def load_bin_file(data, filepath, filename):
+def load_bin_file(data, filename, filepath=pathlib.Path.cwd().__str__()):
     with open(filepath + "/" + filename, 'rb') as file:
         pickle.load(data, file)
 
